48681808aba819accfe87b7364071d3886970f19
Surjeet Singh Aneja <s.s8392898116@gmail.com>
2024-01-09 21:52:04 +0530

    feat: common things to know before writing your first react app.

---
+++ Let's get started/thingsToKnow.js
@@ -0,0 +1,32 @@
+/* Q1. What is emmet?
+Ans. It is a plugin or a developer tool-kit for the developers to make your work faster and sustainable in HTML and CSS workflows. 
+
+
+Q2. Difference between Library and Framework?
+Ans. Library: Library are those which target specific functionality. It does not have inbuilt capabilty to develop a complete applications. Example: React is a library, which helps to make reactive websites but still it does not have all the things needed to be a framework, like automated testing, creating forms.
+
+Framework: Framework is something that provides you every tool needed to develop a complete full-scale application without the requirement of any external tool.
+
+
+Q3. What is CDN? And why do we use it?
+Ans. CDN stands for Content Delivery Network / Content Distribution Network. It is a distributed groups of servers that caches the content to the end users.
+It is used to provide quick transfer of assets needed for loading internet content, like HTML pages, JS files, CSS Stylesheets.
+
+Q4. Why is react known as react?
+Ans. It is named "React" because it was made for the developers to make fast and responsive webpages or "reactive" webpages.
+In React when the data get changed the component in which that data is required will automatically gets updated which makes it seamless on user-interface.
+
+Q4. What is crossorigin in script tag?
+Ans. Crossorigin attribute sets the mode of request to an HTTP CORS request.
+
+CORS stands for crossorigin resource sharing it helps to fetch the data from different origins.
+Example: Suppose surjeetsingh.com want some data from google.com this is where CORS comes in which helps to laod the data from other domain.
+
+
+Q5. What is the difference between react and reactDOM?
+Ans. React: Where react is a JS library used to build user-interface. All the components,hooks,classes,elements,etc that we use comes from react.
+
+ReactDOM: Whereas ReactDOM helps all the react components to show up / rendering the interface in the broser DOM. Whenever we use render, findDomNode() we are using ReactDom.
+ 
+*/
+ 
