43abea9c62bef1b7666fdacb86951601f031ded3
Surjeet Singh Aneja <s.s8392898116@gmail.com>
2024-01-10 17:37:10 +0530

    Creating Hello World program using react CDN links.

--- Let's get started/App.js
+++ Let's get started/App.js
@@ -1,13 +1,31 @@
-// const heading = React.createElement("h1",{},"Hello World in React")
 
-const nestedEle = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I am an h1 "),
-    React.createElement("h2", {}, "I am an h1 "), 
-  ])
-);
+// The below code is Hello World using DOM manipulation without using react CDN links
+/* 
+const heading = document.getElementById('root')
 
+const createEle = document.createElement('h1');
+const node = document.createTextNode('Hello world');
+createEle.appendChild(node);
+
+heading.appendChild(createEle); */
+
+
+// ---------------------------------------------------------
+
+
+// Here we will see how to create Hello World program using React by importing CDN links in html file.
+
+// Firstly, creating a variable heading and using react createElement method.
+
+// createElement consists of 3 parameters 1.type of element 2.An object for attributes and props. 
+// 3. children of the element which can be another react element or string, number,etc.
+
+const heading = React.createElement("h1",{},"Hello World in React")
+
+// then we will create the root for our react to render the heading.
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(nestedEle);
+
+// then by using render method we make heading render on browser.
+root.render(heading);
+
+
