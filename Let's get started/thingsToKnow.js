facdbc24def4c50dde622719fbbb660b8138b5c4
Surjeet Singh Aneja <s.s8392898116@gmail.com>
2024-01-10 16:39:02 +0530

    things to know

--- Let's get started/thingsToKnow.js
+++ Let's get started/thingsToKnow.js
@@ -16,17 +16,28 @@ Q4. Why is react known as react?
 Ans. It is named "React" because it was made for the developers to make fast and responsive webpages or "reactive" webpages.
 In React when the data get changed the component in which that data is required will automatically gets updated which makes it seamless on user-interface.
 
-Q4. What is crossorigin in script tag?
+Q5. What is crossorigin in script tag?
 Ans. Crossorigin attribute sets the mode of request to an HTTP CORS request.
 
 CORS stands for crossorigin resource sharing it helps to fetch the data from different origins.
 Example: Suppose surjeetsingh.com want some data from google.com this is where CORS comes in which helps to laod the data from other domain.
 
 
-Q5. What is the difference between react and reactDOM?
+Q6. What is the difference between react and reactDOM?
 Ans. React: Where react is a JS library used to build user-interface. All the components,hooks,classes,elements,etc that we use comes from react.
 
 ReactDOM: Whereas ReactDOM helps all the react components to show up / rendering the interface in the broser DOM. Whenever we use render, findDomNode() we are using ReactDom.
  
+
+Q7. What is difference between react.development.js and react.production.js files via CDN?
+Ans. The react.development.js is used for the debugging and showing detailed error messages for the smooth development environment. File size is larger than production and the redabilty of the code is better in development.
+
+Whereas react.production.js is basically a minified/compressed version of the project. Its file size is much less than development but the redability is not developer friendly.
+
+
+Q8. What is async and defer attributes in script tag?
+Ans. async: In this attribute while the HTML is parsing the script are fetched along with it. After the script are fetched just after it the parsing stops and the scripts are executed and after the execution the parsing again starts.
+
+defer: In this attrbute the HTML paring goes on and along with it the scripts are fetched but the exection of the scripts are only done when the parsing gets completed. 
 */
  
